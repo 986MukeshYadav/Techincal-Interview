c9eb80e6fef6da32e65a1bdbc94ac40a6e00f5ba
Mukesh <myadav986@rku.ac.in>
2024-08-23 01:08:28 +0530

    more quetion

---
+++ LeetCode/06_AddTwoNumbers.js
@@ -0,0 +1,16 @@
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807
+
+const addTwoNumbers=(l1, l2)=>{
+   l1=l1.reverse().join("")
+   l2=l2.reverse().join("")
+   
+let sum = Number(l1) + Number(l2)
+
+  let sum1=String(sum).split("").reverse().map(Number)
+  
+  return sum1
+}
+
+console.log(addTwoNumbers([1, 2, 3], [4, 5, 6])); //output: [ 5, 7, 9 ]
---
+++ company question/Romain.js
@@ -0,0 +1,23 @@
+const missingnumber=(arr)=>{
+    
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=0;j<arr.length-1-i;j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        } 
+        
+    }
+    let postivenumber = 1;
+    for(let i=0;i<arr.length;i++){
+    if(arr[i] === postivenumber){
+        postivenumber++;
+    }
+    }
+    return postivenumber;
+}
+
+console.log(missingnumber([1,2,0])); // Output: 3
+console.log(missingnumber([9,6,4,2,3,5,7,0,1])); // Output: 8
